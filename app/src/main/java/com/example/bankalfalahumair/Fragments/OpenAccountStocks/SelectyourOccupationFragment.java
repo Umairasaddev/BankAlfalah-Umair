71ada3937b02acc4d78cf467e45ca982c5f22329
Umair Asad Khan <umairasad001@gmail.com>
2023-06-16 16:55:29 +0500

    Eighth Commit

--- app/src/main/java/com/example/bankalfalahumair/Fragments/OpenAccountStocks/SelectyourOccupationFragment.java
+++ app/src/main/java/com/example/bankalfalahumair/Fragments/OpenAccountStocks/SelectyourOccupationFragment.java
@@ -12,10 +12,10 @@ import com.example.bankalfalahumair.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ResidenceFragment#newInstance} factory method to
+ * Use the {@link SelectyourOccupationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ResidenceFragment extends Fragment {
+public class SelectyourOccupationFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class ResidenceFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ResidenceFragment() {
+    public SelectyourOccupationFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class ResidenceFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ResidenceFragment.
+     * @return A new instance of fragment SelectyourOccupationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ResidenceFragment newInstance(String param1, String param2) {
-        ResidenceFragment fragment = new ResidenceFragment();
+    public static SelectyourOccupationFragment newInstance(String param1, String param2) {
+        SelectyourOccupationFragment fragment = new SelectyourOccupationFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class ResidenceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_residence, container, false);
+        return inflater.inflate(R.layout.fragment_selectyour_occupation, container, false);
     }
 }
